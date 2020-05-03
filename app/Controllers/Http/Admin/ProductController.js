08a0d094077b02dbe889cae4f48f6976afd7294f
Ningaanza Remy <remimaradona@gmail.com>
2020-05-03 02:08:02 -0400

    adding new product form view,inserting new productsinto sql and display products

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -3,7 +3,7 @@ const Database = use('Database')
 const sanitize = use('sqlstring')
 
 class ProductController {
-  async index({view,request, response}){
+  async index({view,request,response}){
     try {
       let allProducts= await Database.raw(`
         SELECT products.id,
@@ -33,8 +33,12 @@ ORDER BY created_at ASC
      try {
        const post= request.post()
        await Database.raw(`
-         INSERT INTO products(title,sku,material,description,brand_id,qty, size, user_id)
-         values('${post.title}', '${post.sku}', '${post.material}','${post.description}', 1, '${post.qty}','${post.size}', 1)
+         INSERT INTO products(title,sku,img_url,material,description,brand_id,qty, size, user_id)
+         values(
+           '${post.title}',
+           '${post.sku}',
+           '${post.img_url}',
+           '${post.material}','${post.description}', 1, '${post.qty}','${post.size}', 1)
          `)
          return response.redirect('/admin/products')
      } catch (error) {
@@ -47,11 +51,58 @@ ORDER BY created_at ASC
   create({view,request, response}){
     return view.render('admin/products/create')
   }
-  show({view,request, response}){
-    return view.render('admin/products/show')
+  async show({view,request, response, params}){
+
+    try {
+      let product= await Database.raw(`
+        SELECT products.id,
+products.title, products.sku,products.img_url,products.description,brands.title as brand,
+concat(users.f_name, ' ' ,users.l_name) as user,
+ products.material,products.qty, products.size, products.user_id,products.created_at
+FROM products
+INNER JOIN  brands
+ON products.brand_id = brands.id
+INNER JOIN  users
+ON products.user_id = users.id
+WHERE products.id = ${params.id}
+ORDER BY created_at ASC
+LIMIT 1
+        `)
+        product = product[0][0]
+
+        return view.render('admin/products/show',{product})
+    } catch (error) {
+      console.log(error)
+       return response.redirect('back') //`<h1 style="color: red">THERE WAS AN ERROR</h1><h3>${error.sqlMessage}</h3>`
+
+
+    }
   }
-  edit({view,request, response}){
-    return view.render('admin/products/edit')
+  async edit({view,request, response, params}){
+    try {
+      let product= await Database.raw(`
+        SELECT products.id,
+products.title, products.sku,products.img_url,products.description,brands.title as brand,
+concat(users.f_name, ' ' ,users.l_name) as user,
+ products.material,products.qty, products.size, products.user_id,products.created_at
+FROM products
+INNER JOIN  brands
+ON products.brand_id = brands.id
+INNER JOIN  users
+ON products.user_id = users.id
+WHERE products.id = ${params.id}
+ORDER BY created_at ASC
+LIMIT 1
+        `)
+        product = product[0][0]
+
+        return view.render('admin/products/edit',{product})
+    } catch (error) {
+      console.log(error)
+       return response.redirect('back') //`<h1 style="color: red">THERE WAS AN ERROR</h1><h3>${error.sqlMessage}</h3>`
+
+
+    }
   }
   update({request, response}){
 
